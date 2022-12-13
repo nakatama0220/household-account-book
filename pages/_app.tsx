63d72eff0e19584072de4365dddd3125fdf7e9a9
nakamari <yuuki.0200doglove@gmail.com>
2022-12-13 21:02:45 +0900

    feat: 環境構築

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Global } from '@emotion/react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { global } from '../styles/global';
+import { reset } from '../styles/reset';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}>
+      <Global styles={global} />
+      <Global styles={reset} />
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
+
+export default MyApp;
