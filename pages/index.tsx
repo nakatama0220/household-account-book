c320a56cc87d7718aadbed9bc665c4f01dcf8ea4
nakamari <yuuki.0200doglove@gmail.com>
2022-12-13 22:15:43 +0900

    feat: a

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import { Layout } from '../src/template/Layout';
 
 const Home = () => {
-  const session = useSession();
-  const supabase = useSupabaseClient();
-
   return (
-    <>
-      {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-      ) : (
-        <Layout>
-          <span>demo</span>
-        </Layout>
-      )}
-    </>
+    <Layout>
+      <span>demo</span>
+    </Layout>
   );
 };
 
