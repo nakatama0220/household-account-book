f448f224e35922009e0202a3b4189d85b74080d0
nakamari <yuuki.0200doglove@gmail.com>
2022-12-20 11:00:59 +0900

    feat: AuthContextProviderでWrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import { AuthContextProvider } from '../src/template/AuthContextProvider';
 import { global } from '../styles/global';
 import { reset } from '../styles/reset';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Global styles={global} />
-      <Global styles={reset} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthContextProvider>
+      <ChakraProvider>
+        <Global styles={global} />
+        <Global styles={reset} />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthContextProvider>
   );
 }
 
