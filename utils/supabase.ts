3fccf2cf7497f35d09348dbb0478e4d075d44c47
nakatama <yuuki.0220doglove@gmail.com>
2023-01-30 23:34:52 +0900

    feat: aa

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,6 @@
-// import { createClient } from '@supabase/supabase-js';
-
-// export default createClient(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-// );
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
